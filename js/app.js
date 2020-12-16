e5abf6cbb460fb17ce2c5ba923c6657800d7538f
Quynh <quynhdongikn@gmail.com>
2020-12-16 22:01:52 +0700

    Correct

--- js/app.js
+++ js/app.js
@@ -46,33 +46,43 @@ for (let i = 0; i < sections.length; i++) {
   li.appendChild(btn[i]);
   document.getElementById('navbar__list').appendChild(li);
 }
-// Add class 'active' to section when near top of viewport !!!!! PLEASE SEND HELP TT TT
+// Add class 'active' to section when near top of viewport
 function addClass(current) {
   let currentSectionId = current.toLowerCase().replace(" ", "");
   let currentSection = document.getElementById(currentSectionId);
-  if(isInViewPort(currentSection)){
+  if(isInViewport(currentSection)){
     currentSection.classList.add("your-active-class");
-  } else {
-    currentSection.classList.remove("your-active-class");
   }
-
 }
 // Scroll to anchor ID using scrollTO event
 for (let i = 0; i < sections.length; i++) {
   btn[i].classList.add("menu__link");
   document.querySelectorAll(".menu__link")[i].addEventListener("click", function handleClick() {
     let buttonInnerHTML = this.innerHTML;
-    scrollTo(buttonInnerHTML);
     addClass(buttonInnerHTML);
+    scrollTo(buttonInnerHTML);
   });
 }
 
+//Scroll to the top
+
+const toTop = document.createElement("button");
+toTop.textContent = "Scroll to the top";
+toTop.id = "toTop";
+toTop.setAttribute("onclick", "toTopFunction()");
+document.body.appendChild(toTop);
+document.getElementById("toTop").style.cssText = "position: fixed; bottom: 5vh; left: 45%;";
+
+
+function toTopFunction() {
+  document.documentElement.scrollTop = 0;
+  document.body.scrollTop = 0;
+}
 /**
  * End Main Functions
  * Begin Events
  *
  */
-
 
 // Scroll to section on link click
 function scrollTo(section) {
@@ -81,7 +91,19 @@ function scrollTo(section) {
       sections[i].scrollIntoView({
         behavior: "smooth"
       });
+      setTimeout(function(){
+     document.getElementById("toTop").style.visibility = "hidden";
+      document.getElementsByTagName("nav")[0].style.visibility = "hidden";
+      }, 1000);
     }
   }
 }
-// Set sections as active
+// set time out
+
+window.addEventListener('scroll', function() {
+  setTimeout(function(){
+
+  document.getElementById("toTop").style.visibility = "visible";
+  document.getElementsByTagName("nav")[0].style.visibility = "visible";
+}, 0);
+});
